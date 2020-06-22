2baa048eb01280dea00a961deedd6e599ac90378
andrewing <john_andrew_ing@dlsu.edu.ph>
2020-06-23 01:59:41 +0800

    redeploy

---
+++ redeploy.bat
@@ -0,0 +1,4 @@
+echo "fryo" >> redeploy.txt
+git add .
+git commit -m "redeploy"
+git push
--- util/helper/gdrive.js
+++ util/helper/gdrive.js
@@ -1,4 +1,4 @@
 export const processLinkGdrive = link => {
   const regex = /(?<=https:\/\/drive.google.com\/file\/d\/\s*).*(?=\s*\/.*)/g
-  return `https://drive.google.com/uc?export=view&id=${link.match(regex)}`
+  return `https://drive.google.com/uc?export=view&id=${link?.match(regex)}`
 }
